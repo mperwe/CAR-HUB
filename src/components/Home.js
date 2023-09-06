558dc52d139d4ecbfe6918bb0e56e9b04bff260b
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-09-06 20:51:42 +0200

    adding the solar and accessory section

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,43 +10,57 @@ function Home() {
                 backgroundImg="model-s.jpg"
                 leftBtnText="Custom order" LeftButton
                 rightBtnText="Existing invetory"
-                />
+            />
             <Section
                 title="Model Y"
                 description="Order Online for Touchless Delivery"
                 backgroundImg="model-y.jpg"
-                leftBtnText="Custom order" LeftButton
+                leftBtnText="Custom order"
                 rightBtnText="Existing invetory"
-                />    
+            />
             <Section
                 title="Model 3"
                 description="Order Online for Touchless Delivery"
                 backgroundImg="model-3.jpg"
-                leftBtnText="Custom order" LeftButton
+                leftBtnText="Custom order"
                 rightBtnText="Existing invetory"
-                />
+            />
+
             <Section
-                title="Model X"
-                description="Lowest "
-                backgroundImg="model-x.jpg"
-                leftBtnText="Custom order" LeftButton
-                rightBtnText="Existing invetory"
-                />
-            
-            <Section 
                 title="Model X"
                 description="Order Online for Touchless Delivery"
                 backgroundImg="model-x.jpg"
-                leftBtnText="Custom order" LeftButton
+                leftBtnText="Custom order"
                 rightBtnText="Existing invetory"
-            
+
             />
 
-    
+            <Section
+                title="Lowest Cost Solar Panels in America"
+                description="Money-Back guarantee "
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="Order now"
+                rightBtnText="Learn more"
+            />
+
+            <Section
+                title="Solar for New Roofs"
+                description="Solar Roofs costs Less than a New Roof Plus Solar Panles"
+                backgroundImg="solar-roof.jpg"
+                leftBtnText="Order now"
+                rightBtnText="Learn more"
+            />
+             <Section
+                title="Accessories"
+                description=" "
+                backgroundImg="accessories.jpg"
+                leftBtnText="Shop now"
+               
+            />
         </Container>
     )
 }
- 
+
 export default Home
 
 const Container = styled.div`
